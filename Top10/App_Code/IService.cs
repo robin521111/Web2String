c4dbe0670b163a4c95d1a0596d5be15a429c2ab8
luhaibin <luhaibin731@gmail.com>
2024-06-20 17:33:05 +0800

    Add files via upload
    top10

---
+++ Top10/App_Code/IService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的接口名“IService”。
+[ServiceContract]
+public interface IService
+{
+
+	[OperationContract]
+	string GetData(int value);
+
+	[OperationContract]
+	[WebGet(UriTemplate = "Top10?content={content}")]
+	string[] Top10Words(string content);
+
+	// TODO: 在此添加您的服务操作
+}
+
+// 使用下面示例中说明的数据约定将复合类型添加到服务操作。
+
---
+++ Top10/App_Code/Service.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.UI.WebControls;
+
+// 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、服务和配置文件中的类名“Service”。
+public class Service : IService
+{
+	public string GetData(int value)
+	{
+		return string.Format("You entered: {0}", value);
+	}
+
+	public string[] Top10Words(string content)
+	{
+        
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            string[] filtedwords = content.Split(' ');
+            foreach (string word in filtedwords)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount[word] = 1;
+                }
+            }
+
+            // 按单词出现次数降序排序
+            List<KeyValuePair<string, int>> sortedWords = wordCount.OrderByDescending(w => w.Value).ToList();
+
+            // 获取前10个单词
+            List<string> top10Words = sortedWords.Take(10).Select(w => w.Key).ToList();
+
+            return top10Words.ToArray();
+       
+        
+    }
+
+	
+}
