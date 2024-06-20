7d1d65dee38513c478e5daa67b9c8e2dd2cf6421
luhaibin <luhaibin731@gmail.com>
2024-06-20 17:34:25 +0800

    Add files via upload
    FilterWordrestful

---
+++ FilterWordRestful/Controllers/FilterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilterWordRestful.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WordFilterController : ControllerBase
+    {
+        private static readonly string[] stopwords = new[]
+       {
+
+            "a", "an", "in", "on", "the", "is", "are", "am"
+       };
+
+
+        private readonly ILogger<WordFilterController> _logger;
+
+        public WordFilterController(ILogger<WordFilterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "WordFilter")]
+
+        public string WordFilterRestful(string content)
+        {
+
+            return content;
+        }
+    }
+}
---
+++ FilterWordRestful/Models/WordFilter.cs
@@ -0,0 +1,13 @@
+﻿namespace FilterWordRestful.Models
+{
+    public class WordFilter
+    {
+        public string? Content { get; set; }
+
+        public string? Name { get; set; } = null;
+
+
+
+
+    }
+}
